833258ea3c4d5b1b00e3d3cb592393f3436c3f06
Filipp <gladishevfilipp@gmail.com>
2025-07-09 22:01:07 +0300

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/exhibition/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
 })
